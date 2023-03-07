1b1d67ebca5fe9e13189b3c3f2dd99f9f32a99c6
Vitalii-Prokopenko <doc8259@gmail.com>
2023-03-07 19:07:01 +0200

    gallery simplelightbox added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Gallery markup
+
+const imageGallery = document.querySelector('.gallery');
+let markup = '';
+
+galleryItems.forEach(galleryItem => {
+  let galleryElement = document.createElement('a');
+  let galleryImage = document.createElement('img');
+
+  galleryElement.classList.add('gallery__item');
+  galleryImage.classList.add('gallery__image');
+
+  galleryElement.setAttribute('href', galleryItem.original);
+  galleryImage.setAttribute('src', galleryItem.preview);
+  galleryImage.setAttribute('alt', galleryItem.description);
+
+  markup += `<${galleryElement.localName} class="${galleryElement.className}" href="${galleryElement.href}"><${galleryImage.localName} class="${galleryImage.className}" src="${galleryImage.src}" alt="${galleryImage.alt}"/></${galleryElement.localName}>`;
+});
+
+imageGallery.insertAdjacentHTML('afterbegin', markup);
+
+// Creation of SimpleLightbox object
+
+const mySimpleLightBox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// Event listener on div.gallery and modal window of SimpleLightbox
+
+const handleSimpleLightBox = event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+};
+
+imageGallery.addEventListener('click', handleSimpleLightBox);
+
+// console.log(galleryItems);
