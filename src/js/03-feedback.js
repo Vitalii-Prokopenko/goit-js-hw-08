1416ff07fd3015c9c0c089e25019d0a1b332f287
Vitalii-Prokopenko <doc8259@gmail.com>
2023-03-11 19:14:07 +0200

    update 03 feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+// form.elements.email.removeAttribute('autofocus');
+
+const FORM_STATE_STORAGE_KEY = 'feedback-form-state';
+
+let formSavedData = {};
+
+console.log(localStorage.getItem(FORM_STATE_STORAGE_KEY));
+
+if (localStorage.getItem(FORM_STATE_STORAGE_KEY)) {
+  formSavedData = JSON.parse(localStorage.getItem(FORM_STATE_STORAGE_KEY));
+
+  form.elements.email.textContent = formSavedData.emailInputValue;
+  form.elements.message.textContent = formSavedData.messageInputValue;
+  //   console.log(form.elements.email.value);
+}
+console.log(formSavedData);
+
+const handleInput = event => {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  formSavedData.emailInputValue = email.value;
+  formSavedData.messageInputValue = message.value;
+
+  console.log(formSavedData);
+
+  localStorage.setItem(FORM_STATE_STORAGE_KEY, JSON.stringify(formSavedData));
+
+  //   const SaveData = formSavedData => {
+  //     console.log(1);
+  //     localStorage.setItem(FORM_STATE_STORAGE_KEY, JSON.stringify(formSavedData));
+  //   };
+
+  //   myThrottle(formSavedData => {
+  //     console.log(1);
+  //     localStorage.setItem(FORM_STATE_STORAGE_KEY, JSON.stringify(formSavedData));
+  //   }, 500);
+  //   console.log(localStorage.getItem(FORM_STATE_STORAGE_KEY));
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  console.log(formSavedData);
+  localStorage.setItem(FORM_STATE_STORAGE_KEY, '');
+  form.elements.email.textContent = '';
+  form.elements.message.textContent = '';
+};
+
+form.addEventListener('input', handleInput);
+form.addEventListener('submit', handleSubmit);
